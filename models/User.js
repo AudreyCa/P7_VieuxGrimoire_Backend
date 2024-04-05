14d423c018eec4dff0dbe7d3fd4e30cc9b21f2c9
Aud Can <a.cannesson5@gmail.com>
2024-04-05 17:43:39 +0200

    ajout routes(vides) et models

---
+++ models/User.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    _id:{
+        type:mongoose.ObjectId,
+        require:true
+    },
+    email: { 
+        type: String, 
+        required: true 
+    },
+    password: { 
+        type: String, 
+        required: true 
+    }
+});
+
+module.exports = mongoose.model('User', userSchema);

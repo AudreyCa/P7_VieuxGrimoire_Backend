a6960b674ab36017d1ee0cb8b3239a82416418c9
Aud Can <a.cannesson5@gmail.com>
2024-04-12 17:48:25 +0200

    ajout authentification

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,27 +1,51 @@
+const bcrypt = require('bcrypt');
 const User = require('../models/User');
 
 
 // POST : créeation d'un user
 exports.createUser = (req, res) => {
     console.log(req.body);
-    const user = new User({
-        email: req.body.email,
-        password: req.body.password //! Attention, hashage du mot de passe
-    });
-    user.save()
-        .then(() => {
-            console.log("Utilisateur ajoutée en BDD");
-            res.status(201).json({ message: 'Utilisateur crée avec succès !' })
+    bcrypt.hash(req.body.password, 10)
+        .then(hash => {
+            const user = new User({
+                email: req.body.email,
+                password: hash
+            });
+            user.save()
+                .then(() => {
+                    console.log("Utilisateur ajoutée en BDD");
+                    res.status(201).json({ message: 'Utilisateur crée avec succès !' })
+                })
+                .catch(error => {
+                    console.error(error);
+                    res.status(400).json({ error: error.message });
+                });
         })
         .catch(error => {
             console.error(error);
-            res.status(400).json({ error: error.message });
+            res.status(500).json({ error })
         });
 }
 
 // POST : Login (Renvoie un userId et un token)
 exports.authentificateUser = (req, res, next) => {
     console.log(req.body);
-    // Corps de la requete : { email: string, password: string }
-    // Type de réponse attendue : { userId: string, token: string }
+    User.findOne({ email: req.body.email })
+    .then(user => {
+        if (!user) {
+            return res.status(401).json({ message: 'Paire login/mot de passe incorrecte'});
+        }
+        bcrypt.compare(req.body.password, user.password)
+            .then(valid => {
+                if (!valid) {
+                    return res.status(401).json({ message: 'Paire login/mot de passe incorrecte' });
+                }
+                res.status(200).json({
+                    userId: user._id,
+                    token: 'TOKEN'
+                });
+            })
+            .catch(error => res.status(500).json({ error }));
+    })
+    .catch(error => res.status(500).json({ error }));
 }
--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
 
 const userSchema = mongoose.Schema({
     email: { 
@@ -15,5 +16,7 @@ const userSchema = mongoose.Schema({
     collection: 'user',
     versionKey: false
 });
+
+userSchema.plugin(uniqueValidator);
 
 module.exports = mongoose.model('user', userSchema);
